94a5269d9fcb205a3a5aa64053320baf6be17f23
FancyLeaf <michael.carter@tlgcohort.com>
2024-03-20 18:13:53 +0000

    practicing input and print in a simple script

---
+++ lab_input/greeting.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+    ## This is a practice script intended to provide a greeting with the user's name and the day of the week
+def main():
+    # Start with asking for the name and day of the week
+    user_input = input("What is your name? ")
+    day_of_week = input("What day of the week is it? ")
+    # Next we provide a response with the added day of the week and kind greeting
+    print("Hello, " + user_input + "! Happy " + day_of_week + "!" )
+    # Time to test it out
+
+main()
+
